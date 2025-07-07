afdf0a336d555360f3692a8ee2626ab88929a9b6
bereket <bekibirhanu8693@gmail.com>
2025-07-07 09:39:16 +0300

    adding the ai tools

---
+++ src/infrastructure/llm/Mistral_ai.py
@@ -0,0 +1,23 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import HumanMessage
+from src.config import GOOGLE_API_KEY
+from src.infrastructure.llm.llm_interface import LLMInterface
+
+class MistralAi(LLMInterface):
+    """LangChain implementation for MistralAi."""
+    def __init__(self, model: str = "mistral-large-latest"):
+        self.model = ChatMistralAI(
+            model=model,
+            google_api_key=MISTRAL_API_KEY,
+            temperature=0.7,
+            max_output_tokens=1500
+        )
+
+    async def generate_response(self, prompt: str) -> str:
+        try:
+            messages = [HumanMessage(content=prompt)]
+            response = await self.model.ainvoke(messages)
+            return response.content
+        except Exception as e:
+            print(f"Error calling MistralAi via LangChain: {e}")
+            return "Error: Could not get a response from MistralAi."
